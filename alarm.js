e353d6ec1ee62106a4782b4eac84e5bc2b5451a0
yeroc790 <hansenc@ad.cs.southwesternadventist.edu>
2017-03-21 10:57:16 -0500

    Made alternate alarm function
    Takes a time as input eg. 8:30am and calculates the time till that given
    time

--- alarm.js
+++ alarm.js
@@ -3,6 +3,7 @@ var nodeSpotifyWebHelper = require('node-spotify-webhelper');
 var spotify = new nodeSpotifyWebHelper.SpotifyWebHelper();
 var prompt = require('prompt');
 
+//-- start basic functions --
 function setAlarm(arg){
     prompt.get(['hours', 'minutes'], function(err, result){
         if(err){
@@ -15,6 +16,51 @@ function setAlarm(arg){
 
         if(minutes>=0){
             console.log("\n-- Alarm Set to " + result.hours + " hours and " + result.minutes + " minutes from now --");
+            setTimeout(function(){
+                console.log("\n\nGood morning!");
+                unpause();
+            },(1000*60*minutes));
+        }else if(minutes<0){
+            console.log("-- Error setting alarm --");
+        }
+        if(arg=='menu')
+            menu();
+    });
+}
+
+function setAlarm2(arg){
+    console.log("What time would you like to wake up?");
+    prompt.get(['hour', 'minute', 'amPm'], function(err, result){
+        var newHour = Number(result.hour);
+        var newMin = Number(result.minute);
+        var amPm = result.amPm;
+
+        //converts to military time
+        if(result.amPm == 'pm'){
+            newHour += 12;
+        }
+
+        var now = new Date();
+        var nowHour = now.getHours();
+        var nowMin = now.getMinutes();
+        var diffMin = newMin - nowMin;
+        var diffHour = newHour - nowHour;
+
+        if(diffHour<0){
+            diffHour += 24;
+        }
+
+        if(diffMin<0){
+            if(diffHour==0)
+                diffHour = 23;
+            diffMin += 60;
+        }
+
+        var minutes = (diffHour*60)+diffMin;
+
+        if(minutes>=0){
+            console.log("\n-- Alarm Set to " + newHour + ":" + newMin + " --");
+            console.log("That is " + diffHour + " hours and " + diffMin + " minutes from now");
             setTimeout(function(){
                 console.log("\n\nGood morning!");
                 unpause();
@@ -53,9 +99,14 @@ function info(arg){
             return console.error(err);
         }
 
-        console.info('\nCurrently playing:',
-        res.track.artist_resource.name, '-',
-        res.track.track_resource.name + '\n');
+        artist = res.track.artist_resource;
+        track = res.track.track_resource;
+
+        if((artist==null)&&(track==null)){
+            console.info('\nNot currently playing anything\n');
+        }else{
+            console.info('\nCurrently playing:', artist.name, '-', track.name + '\n');
+        }
 
         if(arg=='menu')
             menu();
@@ -65,6 +116,7 @@ function info(arg){
 
 //main logic flow
 //explicitly called once, then recursively until quit is called? sure...that sounds like a plan
+//should check if spotify-web-helper.exe is running, and not either launch Spotify or not accept commands
 menu();
 prompt.start();
 var action;
@@ -84,7 +136,7 @@ function menu(){
                 unpause('menu');
                 break;
             case 'alarm':
-                setAlarm('menu');
+                setAlarm2('menu');
                 break;
             case 'quit':
                 console.log('\n...Shutting down...');
@@ -93,7 +145,6 @@ function menu(){
                 console.log('\nAction is not supported');
                 menu();
                 break;
-
         }
     });
 }
